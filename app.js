eb03a32c2cc9436f4bbf6d2b30162e8b014b9556
majorazero <daniel.cw.hsu@gmail.com>
2018-03-14 16:43:16 -0700

    added test to check for new endpoints as well as adding a cities endpoint in app.js

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ app.get('/',function(req,resp){
   resp.send('OK');
 });
 
+app.get('/cities',function(req,resp){
+  resp.send('OK');
+});
+
 //app.listen(3000);
 module.exports = app; //wrote it like this so our test.js can access it as a module
--- test.js
+++ test.js
@@ -2,11 +2,24 @@
 var request = require('supertest');
 var app = require('./app');
 
-//following is a format that we'll design our test after.
-request(app)
-  .get('/')
-  .expect(200)
-  .end(function(error){
-    if(error) throw error;
-    console.log("Done");
+//following is mocha code
+
+describe('Requests to the root path',function(){
+  it('Returns a 200 status code',function(done){
+    //following is a format that we'll design our test after.
+    request(app)
+      .get('/')
+      .expect(200)
+      .end(function(error){
+        if(error) throw error;
+        done();
+      });
   });
+});
+describe('Listing cities',function(){
+  it('Returns a 200 status code',function(done){
+    request(app)
+    .get('/cities') //searches for the cities endpoint (address)
+    .expect(200,done); //alternative way of writing above code
+  });
+});
